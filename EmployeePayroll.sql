6067fd24762c4abf67efdc02420634cc088523b7
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-04 20:50:09 +0530

    Added: Insert data into Payroll Service Data Base

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -6,3 +6,6 @@ use pay_roll;
 ---------- UC2 ----------
 create table employee_payroll (emp_id int unsigned not null auto_increment,emp_name varchar(50) not null,salary double not null,startdate date not null,primary key(emp_id));
 describe employee_payroll;
+
+----------- UC3 ------------
+insert into employee_payroll (emp_name,salary,startdate) values ('ramesh','25000','2022-01-12'),('chandu','30000','2021-12-21'),('vinay','27000','2020-05-1');
