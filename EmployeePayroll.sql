dcc721fbedf7636bb30807516a2cfb606c041375
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-04 20:45:13 +0530

    Added: Create  Employee Payroll Service Data Base Table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -2,3 +2,7 @@
 show databases;
 create database pay_roll;
 use pay_roll;
+
+---------- UC2 ----------
+create table employee_payroll (emp_id int unsigned not null auto_increment,emp_name varchar(50) not null,salary double not null,startdate date not null,primary key(emp_id));
+describe employee_payroll;
